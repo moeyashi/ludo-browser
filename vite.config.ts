581bc06630a59e08c67ef16ac0cb747571fac0ee
Ren Adachi <49895682+moeyashi@users.noreply.github.com>
2025-11-30 22:16:54 +0900

    fix: workersへのデプロイもbuild前提だった
    buildでclientとserverどちらもdeployするように
    
    Signed-off-by: Ren Adachi <49895682+moeyashi@users.noreply.github.com>

--- vite.config.ts
+++ vite.config.ts
@@ -1,22 +1,17 @@
 import { fileURLToPath, URL } from 'node:url'
 
 import { cloudflare } from '@cloudflare/vite-plugin'
-import { defineConfig, UserConfig } from 'vite'
+import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import vueJsx from '@vitejs/plugin-vue-jsx'
 import vueDevTools from 'vite-plugin-vue-devtools'
 
 // https://vite.dev/config/
-export default defineConfig(({ command }): UserConfig => {
-  // Only enable the Cloudflare Workers plugin during dev for now
-  const plugins = command === 'serve' ? [cloudflare()] : []
-
-  return {
-    plugins: [...plugins, vue(), vueJsx(), vueDevTools()],
-    resolve: {
-      alias: {
-        '@': fileURLToPath(new URL('./src', import.meta.url)),
-      },
+export default defineConfig({
+  plugins: [cloudflare(), vue(), vueJsx(), vueDevTools()],
+  resolve: {
+    alias: {
+      '@': fileURLToPath(new URL('./src', import.meta.url)),
     },
-  }
+  },
 })
