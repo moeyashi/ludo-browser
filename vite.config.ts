66043dc8652c2ee8019264eefe661e2309841c48
Ren Adachi <49895682+moeyashi@users.noreply.github.com>
2025-11-30 22:09:06 +0900

    fix: mergeConfigにUserConfigFnObjectを渡せるように
    Signed-off-by: Ren Adachi <49895682+moeyashi@users.noreply.github.com>

--- vite.config.ts
+++ vite.config.ts
@@ -1,13 +1,13 @@
 import { fileURLToPath, URL } from 'node:url'
 
 import { cloudflare } from '@cloudflare/vite-plugin'
-import { defineConfig } from 'vite'
+import { defineConfig, UserConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import vueJsx from '@vitejs/plugin-vue-jsx'
 import vueDevTools from 'vite-plugin-vue-devtools'
 
 // https://vite.dev/config/
-export default defineConfig(({ command }) => {
+export default defineConfig(({ command }): UserConfig => {
   // Only enable the Cloudflare Workers plugin during dev for now
   const plugins = command === 'serve' ? [cloudflare()] : []
 
